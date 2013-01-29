6e2990b49c74318018e6ff8310f7686f07b2fa85
garima sharma <sharma_garima27@yahoo.com>
2013-01-29 12:03:14 +0530

    first commit

---
+++ test/src/com/change/kranti/MyActivityTest.java
@@ -0,0 +1,10 @@
+package com.change.kranti;
+
+import android.test.ActivityInstrumentationTestCase2;
+public class MyActivityTest extends ActivityInstrumentationTestCase2<MyActivity> {
+
+    public MyActivityTest() {
+        super("com.change.kranti", MyActivity.class);
+    }
+
+}
