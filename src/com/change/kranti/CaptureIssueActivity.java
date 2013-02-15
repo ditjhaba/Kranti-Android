265d652bcc8d0d1385d7791c1605fea10f1bd8d2
NazneenRupawalla <nazneenr@thoughtworks.com>
2013-02-15 23:32:10 +0530

    Removed unused imports

--- src/com/change/kranti/CaptureIssueActivity.java
+++ src/com/change/kranti/CaptureIssueActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-import com.kranti.location.GPSLocation;
 import com.kranti.location.LocationChangeTrigger;
 import repository.IssueRepository;
 
