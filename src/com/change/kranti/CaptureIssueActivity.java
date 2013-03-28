a5b59d7fa4e28acdc309625f0b6fec975ac10afd
Rohini Kulkarni <rohinik@thoughtworks.com>
2013-03-28 18:24:18 +0530

    Removed code related to image view

--- src/com/change/kranti/CaptureIssueActivity.java
+++ src/com/change/kranti/CaptureIssueActivity.java
@@ -2,17 +2,15 @@ package com.change.kranti;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 import com.kranti.location.LocationChangeTrigger;
 import repository.IssueRepository;
 
-import java.io.*;
+import java.io.File;
 
 public class CaptureIssueActivity extends Activity {
     private static int count = 0;
@@ -40,39 +38,11 @@ public class CaptureIssueActivity extends Activity {
         if (requestCode == IMAGE_CAPTURE && resultCode == Activity.RESULT_OK && data != null) {
 
             ImageData = data;
-            showImage();
             return;
         }
         Toast.makeText(this, "Issue Image not captured !!", Toast.LENGTH_LONG).show();
     }
 
-    private Bitmap showImage() {
-        ImageView issueImageView = (ImageView) findViewById(R.id.issue_image_capture);
-        Bitmap issueImageBitmap = (Bitmap) ImageData.getExtras().get("data");
-        issueImageView.setImageBitmap(issueImageBitmap);
-        return issueImageBitmap;
-    }
-
-    private String storeImage() {
-        fileName = "Issue_" + count++ + ".png";
-        Toast.makeText(this, "Issue Image was captured", Toast.LENGTH_LONG).show();
-        File outputFile = new File(createDirectory(), fileName);
-        Bitmap issueImageBitmap = showImage();
-        ByteArrayOutputStream imageBytes = new ByteArrayOutputStream();
-        issueImageBitmap.compress(Bitmap.CompressFormat.PNG, 90, imageBytes);
-        try {
-            outputFile.createNewFile();
-            FileOutputStream stream = new FileOutputStream(outputFile);
-            stream.write(imageBytes.toByteArray());
-            return outputFile.getPath();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 
     private File createDirectory() {
         File krantiDir = new File("/sdcard/kranti/");
