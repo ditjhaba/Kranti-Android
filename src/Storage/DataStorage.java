abbf0c28f31e7cf3760c0dfd60a7fed30cc917f3
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2013-05-14 17:19:41 +0530

    Final Commit.

--- src/Storage/DataStorage.java
+++ src/Storage/DataStorage.java
@@ -1,13 +1,11 @@
 package Storage;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
 import model.Issue;
-
-import java.util.ArrayList;
-import java.util.List;
+import static android.provider.BaseColumns._ID;
 
 
 public class DataStorage extends SQLiteOpenHelper {
@@ -15,21 +13,30 @@ public class DataStorage extends SQLiteOpenHelper {
     private static final String TITLE_COL = "title";
     private static final String DESCRIPTION_COL = "description";
     private static final String DB_NAME = "kranti.db";
+    private static final String DATABASE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" + _ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TITLE_COL
+            + " TEXT NOT NULL ," + DESCRIPTION_COL + " TEXT );";
+
 
     public DataStorage(Context context) {
         super(context, DB_NAME, null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) { //create your database
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    public void store(Issue issue) {
+        SQLiteDatabase db = getWritableDatabase();
+        SQLiteStatement statement = db.compileStatement("insert into " + TABLE_NAME + " (" + TITLE_COL + "," + DESCRIPTION_COL + ") values ( ?, ? )");
+        statement.bindString(1, issue.getTitle());
+        statement.bindString(2, issue.getDescription());
+        statement.executeInsert();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    }
-
-
-    public void store(Issue issue) { //insert into your database
     }
 
 //    public List<Issue> get() {
--- src/com/change/kranti/CaptureIssueActivity.java
+++ src/com/change/kranti/CaptureIssueActivity.java
@@ -2,12 +2,17 @@ package com.change.kranti;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 import repository.IssueRepository;
 
 public class CaptureIssueActivity extends Activity {
+    private static final int IMAGE_CAPTURE = 0;
     private IssueRepository issueRepository;
 
     @Override
@@ -15,6 +20,21 @@ public class CaptureIssueActivity extends Activity {
         super.onCreate(savedInstanceState);
         issueRepository = new IssueRepository(getApplicationContext());
         setContentView(R.layout.create);
+    }
+
+    public void captureIssueImage(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IMAGE_CAPTURE && resultCode == Activity.RESULT_OK && data != null) {
+
+            showImage((Bitmap)data.getExtras().get("data"));
+            return;
+        }
+        Toast.makeText(this, "Issue Image not captured !!", Toast.LENGTH_LONG).show();
     }
 
 
@@ -25,7 +45,10 @@ public class CaptureIssueActivity extends Activity {
         String issueTitle =  title.getText().toString();
         String issueDescription =  description.getText().toString();
         issueRepository.createIssue(issueTitle,issueDescription);
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+        finish();
+    }
+    private void showImage(Bitmap data) {
+        ImageView issueImageView = (ImageView) findViewById(R.id.captureButton);
+        issueImageView.setImageBitmap(data);
     }
 }
