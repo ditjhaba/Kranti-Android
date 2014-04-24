2fccf1f190355dc5ae0281a30307bdf3e79fe3c3
Ditjhaba <edselemela@gmail.com>
2014-04-24 14:11:23 +0200

    adding an activity and layout for view activity.

---
+++ src/com/change/kranti/ViewIssuesActivity.java
@@ -0,0 +1,14 @@
+package com.change.kranti;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ditjhaba on 2014/04/24.
+ */
+public class ViewIssuesActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view);
+    }
+}
